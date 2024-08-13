4dadfa6954829012688bb8ebfd0d32bf021d186a
gabimottad <gabimottapess@gmail.com>
2024-08-13 00:31:48 -0300

    add

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,30 +1,32 @@
-import { Controller, Post, Body, Res, HttpStatus } from '@nestjs/common';
+import { Controller, Post, Body, HttpStatus, Res } from '@nestjs/common';
 import { AppService } from './app.service';
 import { Response } from 'express';
 
 @Controller()
 export class AppController {
-  constructor(private readonly whatsAppService: AppService) { }
+  constructor(private readonly appService: AppService) { }
 
   @Post('webhook')
   async handleWebhook(@Body() data: any, @Res() res: Response): Promise<Response> {
-    const { id, status, customer } = data;
+    const orderId = data.id;
+    const status = data.status;
+    const customer = data.customer;
     const customerName = `${customer.name} ${customer.lastname}`;
     const customerPhone = customer.cellphone;
 
     const statusMessages = {
-      0: 'recebido',
-      1: 'em preparação',
-      2: 'pronto para entrega',
-      3: 'a caminho',
-      4: 'entregue',
-      5: 'cancelado',
+      0: "recebido",
+      1: "em preparação",
+      2: "pronto para entrega",
+      3: "a caminho",
+      4: "entregue",
+      5: "cancelado",
     };
 
     const statusText = statusMessages[status] || 'atualizado';
-    const message = `Olá ${customerName}, seu pedido #${id} está ${statusText}. Obrigado por comprar conosco!`;
+    const message = `Olá ${customerName}, seu pedido #${orderId} está ${statusText}. Obrigado por comprar conosco!`;
 
-    const isMessageSent = await this.whatsAppService.sendWhatsAppMessage(customerPhone, message);
+    const isMessageSent = await this.appService.sendWhatsappMessage(customerPhone, message);
 
     if (isMessageSent) {
       return res.status(HttpStatus.OK).json({ status: 'success' });
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,41 +1,29 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
-import * as venom from 'venom-bot';
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { Twilio } from 'twilio';
 
 @Injectable()
-export class AppService implements OnModuleInit {
-  private client: venom.Whatsapp | null = null;
+export class AppService {
+  private twilioClient: Twilio;
 
-  async onModuleInit() {
-    try {
-      this.client = await venom.create(
-        'session-name',
-        undefined,
-        undefined,
-        {
-          headless: 'new',
-        }
-      );
-      console.log('WhatsApp client initialized successfully');
-    } catch (error) {
-      console.error('Failed to initialize WhatsApp client:', error);
-    }
+  constructor(private configService: ConfigService) {
+    this.twilioClient = new Twilio(
+      this.configService.get<string>('TWILIO_ACCOUNT_SID'),
+      this.configService.get<string>('TWILIO_AUTH_TOKEN'),
+    );
   }
 
-  async sendWhatsAppMessage(to: string, message: string): Promise<boolean> {
-    if (!this.client) {
-      console.error('WhatsApp client is not initialized');
-      return false;
-    }
-    const formattedNumber = `${to}@c.us`;
-
+  async sendWhatsappMessage(to: string, message: string): Promise<boolean> {
     try {
-      await this.client.sendText(formattedNumber, message);
-      console.log(`Message sent to ${to}: ${message}`);
+      await this.twilioClient.messages.create({
+        body: message,
+        from: this.configService.get<string>('TWILIO_WHATSAPP_NUMBER'),
+        to: `whatsapp:${to}`,
+      });
       return true;
     } catch (error) {
       console.error('Failed to send WhatsApp message:', error);
       return false;
     }
   }
-
 }
