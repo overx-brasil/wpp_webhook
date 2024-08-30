1f3c0622c89565468f0bd6306e3526122d4e891d
gabimottad <gabimottapess@gmail.com>
2024-08-30 00:39:43 -0300

    add

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,16 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import axios from 'axios';
+import { Twilio } from 'twilio';
 
 @Injectable()
 export class AppService {
+  private twilioClient: Twilio;
   private readonly twilioAccountSid: string;
   private readonly twilioAuthToken: string;
+  private readonly twilioWhatsappNumber: string;
 
   constructor(private configService: ConfigService) {
     this.twilioAccountSid =
       this.configService.get<string>('TWILIO_ACCOUNT_SID');
     this.twilioAuthToken = this.configService.get<string>('TWILIO_AUTH_TOKEN');
+    this.twilioWhatsappNumber = this.configService.get<string>(
+      'TWILIO_WHATSAPP_NUMBER',
+    );
+    this.twilioClient = new Twilio(this.twilioAccountSid, this.twilioAuthToken);
   }
 
   public formatPhoneNumber(phone: string): string {
@@ -31,30 +37,29 @@ export class AppService {
   ): Promise<any> {
     const formattedPhone = this.formatPhoneNumber(to);
 
-    const url = `https://api.twilio.com/2010-04-01/Accounts/${this.twilioAccountSid}/Messages.json`;
-
-    const data = new URLSearchParams();
-    data.append('From', `whatsapp:+12292315694`);
-    data.append('To', `whatsapp:${formattedPhone}`);
-    data.append('TemplateSid', 'HXd732b1344f83ffd22a4ab4f73acbb7ae');
-    data.append(
-      'Body',
-      JSON.stringify({
-        1: customerName,
-        2: orderId,
-        3: statusText,
-      }),
-    );
-
     try {
-      const response = await axios.post(url, data, {
-        auth: {
-          username: this.twilioAccountSid,
-          password: this.twilioAuthToken,
+      const message = await this.twilioClient.messages.create({
+        from: `whatsapp:${this.twilioWhatsappNumber}`,
+        to: `whatsapp:${formattedPhone}`,
+        template: {
+          name: 'order_update_notification',
+          language: {
+            code: 'pt_BR',
+          },
+          components: [
+            {
+              type: 'body',
+              parameters: [
+                { type: 'text', text: customerName },
+                { type: 'text', text: orderId },
+                { type: 'text', text: statusText },
+              ],
+            },
+          ],
         },
       });
-      console.log('Mensagem de template enviada com sucesso:', response.data);
-      return response.data;
+      console.log('Mensagem de template enviada com sucesso:', message);
+      return message;
     } catch (error) {
       console.error(
         'Falha ao enviar mensagem de template pelo WhatsApp:',
