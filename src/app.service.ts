e7702c4b1404af750c106cb8612c57fa1f09bcaa
gabimottad <gabimottapess@gmail.com>
2024-08-30 00:58:57 -0300

    add

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,19 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import axios from 'axios';
+import { Twilio } from 'twilio';
 
 @Injectable()
 export class AppService {
-  private readonly twilioAccountSid: string;
-  private readonly twilioAuthToken: string;
-  private readonly twilioWhatsappNumber: string;
+  private readonly twilioClient: Twilio;
 
   constructor(private configService: ConfigService) {
-    this.twilioAccountSid =
-      this.configService.get<string>('TWILIO_ACCOUNT_SID');
-    this.twilioAuthToken = this.configService.get<string>('TWILIO_AUTH_TOKEN');
-    this.twilioWhatsappNumber = this.configService.get<string>(
-      'TWILIO_WHATSAPP_NUMBER',
+    this.twilioClient = new Twilio(
+      this.configService.get<string>('TWILIO_ACCOUNT_SID'),
+      this.configService.get<string>('TWILIO_AUTH_TOKEN'),
     );
   }
 
@@ -35,31 +31,14 @@ export class AppService {
   ): Promise<any> {
     const formattedPhone = this.formatPhoneNumber(to);
 
-    const url = `https://api.twilio.com/2010-04-01/Accounts/${this.twilioAccountSid}/Messages.json`;
-
-    const data = new URLSearchParams();
-    data.append('From', `whatsapp:${this.twilioWhatsappNumber}`);
-    data.append('To', `whatsapp:${formattedPhone}`);
-    data.append('TemplateSid', 'HXd732b1344f83ffd22a4ab4f73acbb7ae');
-    data.append('MessagingServiceSid', 'HXd732b1344f83ffd22a4ab4f73acbb7ae');
-    data.append(
-      'Body',
-      JSON.stringify({
-        1: customerName,
-        2: orderId,
-        3: statusText,
-      }),
-    );
-
     try {
-      const response = await axios.post(url, data, {
-        auth: {
-          username: this.twilioAccountSid,
-          password: this.twilioAuthToken,
-        },
+      const message = await this.twilioClient.messages.create({
+        from: `whatsapp:${this.configService.get<string>('TWILIO_WHATSAPP_NUMBER')}`,
+        to: `whatsapp:${formattedPhone}`,
+        body: `Olá ${customerName}, seu pedido #${orderId} está ${statusText}. Obrigado por comprar conosco!`,
       });
-      console.log('Mensagem de template enviada com sucesso:', response.data);
-      return response.data;
+      console.log('Mensagem de template enviada com sucesso:', message);
+      return message;
     } catch (error) {
       console.error(
         'Falha ao enviar mensagem de template pelo WhatsApp:',
