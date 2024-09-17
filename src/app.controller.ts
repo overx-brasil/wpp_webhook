ce0f51b8c65ed539a10a5c857204268a25886587
gabimottad <gabimottapess@gmail.com>
2024-09-17 19:07:16 -0300

    add

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -30,11 +30,31 @@ export class AppController {
 
     const statusMessages = {
       0: 'pendente',
-      7: 'aceito pela empresa',
+      1: 'concluído',
+      2: 'rejeitado',
+      3: 'motorista chegou no estabelecimento',
       4: 'com a preparação concluída',
-      23: 'em rota de entrega',
+      5: 'rejeitado pela loja',
+      6: 'cancelado pelo motorista',
+      7: 'aceito pela empresa',
+      8: 'aceito pelo motorista',
+      9: 'retirada concluída pelo motorista',
+      10: 'falha na retirada pelo motorista',
       11: 'entregue ao cliente',
+      12: 'pedido não pode ser entregue ao cliente',
+      13: 'pedido antecipado',
+      14: 'pedido não pronto',
+      15: 'retirada concluída pelo cliente',
       16: 'cancelado pelo cliente',
+      17: 'não retirado pelo cliente',
+      18: 'o motorista está chegando na empresa',
+      19: 'o motorista está chegando ao cliente',
+      20: 'cliente está chegando ao negócio',
+      21: 'cliente chegou na loja',
+      22: 'procurando motorista',
+      23: 'em rota de entrega',
+      24: 'motorista aguardando na empresa',
+      25: 'aceito pela empresa',
     };
 
     const statusText = statusMessages[status] || 'atualizado';
